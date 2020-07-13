8a0cc5a2a0d479af7fc432ca76eeafd9fc7cb0e8
kim <uzumeneo@gmail.com>
2020-07-13 16:25:08 +0800

    update

---
+++ source/sftpd/myftpd.c
@@ -0,0 +1,4 @@
+#include  <stdlib.h>     /* strlen(), strcmp() etc */
+#include  <stdio.h>      /* printf()  */
+#include  <sys/types.h>  /* pid_t, u_long, u_short */
+#include  <sys/socket.h> /* struct sockaddr, socket(), etc */
