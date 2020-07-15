82520f61d54342d38de2bdbf041eb4628c471d89
ethannjw <ethannjw@hotmail.com>
2020-07-15 16:27:38 +0800

    Pwd feature working

--- source/myftpd/stream.c
+++ source/myftpd/stream.c
@@ -1,5 +1,5 @@
 /*
- *  File: 	stream.c for assign2 
+ *  File: 	stream.c for assign2
  *  Authors:	Neo Kim Heok (33747085) and Ng Jing Wei (33804877)
  *  Purpose: 	Contains the read/write n, read/write opcode, read/write length
  */
@@ -8,56 +8,81 @@
 #include  <netinet/in.h> /* struct sockaddr_in, htons(), htonl(), */
 #include  "stream.h"
 #include <unistd.h>
-
+/*
 int readn(int fd, char *buf, int bufsize)
 {
-    short data_size;    /* sizeof (short) must be 2 */ 
+    short data_size;     //sizeof (short) must be 2
     int n, nr, len;
 
-    /* check buffer size len */
+    //check buffer size len
     if (bufsize < MAX_BLOCK_SIZE)
-         return (-3);     /* buffer too small */
+         return (-3);      //buffer too small
 
-    /* get the size of data sent to me */
+    // get the size of data sent to me
     if (read(fd, (char *) &data_size, 1) != 1) return (-1);
     if (read(fd, (char *) (&data_size)+1, 1) != 1) return (-1);
-    len = (int) ntohs(data_size);  /* convert to host byte order */ 
+    len = (int) ntohs(data_size);  // convert to host byte order
 
-    /* read len number of bytes to buf */
+    // read len number of bytes to buf
     for (n=0; n < len; n += nr) {
-        if ((nr = read(fd, buf+n, len-n)) <= 0) 
-            return (nr);       /* error in reading */
+        if ((nr = read(fd, buf+n, len-n)) <= 0)
+            return (nr);       // error in reading
     }
-    return (len); 
+    return (len);
+}
+*/
+// code from daniel
+int readn(int sd, char *buf, int nbytes)
+{
+	int nr = 1;
+	int n = 0;
+	for (n=0; (n < nbytes) && (nr > 0); n += nr) {
+		if ((nr = read(sd, buf+n, nbytes-n)) < 0){
+			return (nr);  //read error
+		}
+	}
+	return (n);
 }
 
+/*
 int writen(int fd, char *buf, int nbytes)
 {
-    short data_size = nbytes;     /* short must be two bytes long */
+    short data_size = nbytes;     // short must be two bytes long
     int n, nw;
 
-    if (nbytes > MAX_BLOCK_SIZE) 
-         return (-3);    /* too many bytes to send in one go */ 
+    if (nbytes > MAX_BLOCK_SIZE)
+         return (-3);    // too many bytes to send in one go
 
-    /* send the data size */
-    data_size = htons(data_size);  
-    if (write(fd, (char *) &data_size, 1) != 1) return (-1);      
-    if (write(fd, (char *) (&data_size)+1, 1) != 1) return (-1);       
+    // send the data size
+    data_size = htons(data_size);
+    if (write(fd, (char *) &data_size, 1) != 1) return (-1);
+    if (write(fd, (char *) (&data_size)+1, 1) != 1) return (-1);
 
-    /* send nbytes */
+    // send nbytes
     for (n=0; n<nbytes; n += nw) {
-         if ((nw = write(fd, buf+n, nbytes-n)) <= 0)  
-             return (nw);    /* write error */
-    } 
+         if ((nw = write(fd, buf+n, nbytes-n)) <= 0)
+             return (nw);    // write error
+    }
     return (n);
 }
-
+*/
+// Code from daniel
+int writen(int sd, char *buf, int nbytes)
+{
+	int nw = 0;
+	int n = 0;
+	for (n=0; n < nbytes; n += nw) {
+		if ((nw = write(sd, buf+n, nbytes-n)) <= 0)
+			return (nw); /* write error */
+	}
+  return n;
+}
 
 // Writes one byte char on the socket.
 int write_opcode(int socket_desc, char code)
 {
 	// write the 1 byte code to socket
-	if (write(socket_desc, (char*)&code, 1) != 1) 
+	if (write(socket_desc, (char*)&code, 1) != 1)
 	{
 		return (-1);
 	}
@@ -70,7 +95,7 @@ int read_opcode(int socket_desc, char* code)
 	char data;
 
 	// read 1 byte code from socket
-	if(read(socket_desc,(char *) &data,1) != 1) 
+	if(read(socket_desc,(char *) &data,1) != 1)
 	{
 		return -1;
 	}
@@ -83,13 +108,13 @@ int read_opcode(int socket_desc, char* code)
 int write_length(int socket_desc, int len)
 {
 	// convert to host to network long
-	int data = htonl(len); 
+	int data = htonl(len);
 
-	if (write(socket_desc,&data, 4) != 4) 
+	if (write(socket_desc,&data, 4) != 4)
 	{
 		return (-1);
 	}
-	
+
 	return 1;
 }
 
